5abdb01417a9c8cb7b78e23b842596a232d3a2a2
shanemarvinmay <shanemarvinmay@gmail.com>
2020-01-29 10:31:53 -0600

    Initial Commit

---
+++ Vector/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Vector
+//
+//  Created by Sheldon May Gunlogson on 1/29/20.
+//  Copyright © 2020 Shane May. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
