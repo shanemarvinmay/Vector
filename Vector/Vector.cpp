cac3c46602c3940caeccee49c891c440021a26db
shanemarvinmay <shanemarvinmay@gmail.com>
2020-01-29 20:05:58 -0600

    project done

--- Vector/Vector.cpp
+++ Vector/Vector.cpp
@@ -7,6 +7,7 @@
 //
 #include <iostream>
 #include "Vector.hpp"
+//#include <array>
 
 using namespace std;
 
@@ -19,7 +20,9 @@ template <class DataType> Vector<DataType>::Vector()
 }
 template <class DataType> Vector<DataType>::~Vector()
 {
-    
+//    delete dataItems;
+//    free(capacity);
+//    free(size);
 }
 template <class DataType> void Vector<DataType>::append(const DataType d)
 {
@@ -32,11 +35,11 @@ template <class DataType> void Vector<DataType>::append(const DataType d)
         capacity = 1;
     }
 //    check if there is any capacity : if capacity == 0 double size
-    else if( capacity == 0 )
+    else if( capacity <= 0 )
     {
 //        doubling size
-        int len = size;
-        DataType * temp = new DataType[len*2]();
+        int len = size * 2;
+        DataType * temp = new DataType[len]();
         for(int i = 0; i < len; i++)
         {
             temp[i] = dataItems[i];
@@ -62,27 +65,123 @@ template <class DataType> void Vector<DataType>::append(const DataType d)
 
 template <class DataType> DataType Vector<DataType>::elementAt(const int idx) const
 {
-    try {
-        return dataItems[idx];
-    } catch (int error) {
-        cout << "Accessing idx out of range\n";
-        return -1;
+    if( idx < 0 || idx >= size + capacity)
+    {
+        exit(0);
     }
+    return dataItems[idx];
+//    try {
+//        return dataItems[idx];
+//    } catch (int error) {
+//        cout << "Accessing idx out of range\n";
+//        return -1;
+//    }
+}
+
+template <class DataType> DataType &Vector<DataType>::operator[] (const int idx)
+{
+    if (idx < 0 || idx > size) {
+        cout<<"why would you call and index that you know isn't there?\nCome on now.\n";
+        exit(0);
+    }
+    return dataItems[idx];
+}
+
+template <class DataType> void Vector<DataType>::insertAt (const int idx, const DataType d )  // Insert a new element at the given index
+{
+//    checking to see if the array is too small
+    int len = size + capacity;
+    if(size + 1 > len)
+    {
+        while(len < idx)
+        {
+            len *= 2;
+        }
+        DataType * temp = new DataType[len]();
+        for(int i = 0; i < len; i++)
+        {
+            temp[i] = dataItems[i];
+        }
+        dataItems = temp;
+        capacity = len - size;
+    }
+//    shifting and inserting the new item
+    DataType * copy = new DataType[len]();
+    for(int i = 0; i < idx; i++)
+    {
+        copy[i] = dataItems[i];
+    }
+    copy[idx] = d;
+    for(int i = idx; i < size; i++)
+    {
+        copy[i+1] = dataItems[i];
+    }
+    dataItems = copy;
+//    update size and compacity
+    capacity = len - size;
+    size += 1;
+}
+template <class DataType> int Vector<DataType>::getSize() const  // Return the number of items in the vector
+{
+    return size + capacity;
+}
+template <class DataType> void Vector<DataType>::removeAt(const int idx)  // Remove the data item at the given index
+{
+    int len = size + capacity;
+    DataType * copy = new DataType[len];
+    for(int i = 0; i < len; i++)
+    {
+        if(i != idx)
+        {
+            copy[i] = dataItems[i];
+        }
+    }
+    dataItems = copy;
+}
+template <class DataType> void Vector<DataType>::remove(DataType item) // Remove the given element from the vector
+{
+    int len = size + capacity;
+    for(int i = 0; i < len; i++)
+    {
+        if( dataItems[i] == item )
+        {
+            removeAt(i);
+        }
+    }
+}
+template <class DataType> void Vector<DataType>::clear()
+{
+    capacity = 0;       // the capacity of the vector
+    size = 0;        // Actual number of data item in the vector
+    dataItems = new DataType[size] ();  // Array containing the vector data item
+}
+template <class DataType> bool Vector<DataType>::isEmpty() const
+{
+    return size == 0;
+}
+template <class DataType> bool Vector<DataType>::isFull() const
+{
+    return capacity == 0;
+}
+template <class DataType> int Vector<DataType>::contains(const DataType d) const
+{
+    int len = size + capacity;
+    for(int i = 0; i < len; i++)
+    {
+        if(dataItems[i] == d)
+        {
+            return i;
+        }
+    }
+    return -1;
 }
 /**
 // Vector manipulation operations
-void insertAt (const int index, const DataType item );  // Insert a new element at the given index
-void removeAt (const int index );        // Remove the data item at the given index
-void remove(DataType item);                   // Remove the given element from the vector
 void clear ();                         // Make the vector empty
-
 // Vector status operations
 bool isEmpty () const;                  // Is the Vector empty?
 bool isFull() const;                    // Is the Vector full?
 int  contains(const DataType item) const;    // Does the vector contain the given item? return the index of the item if found, otherwise, return -1.
-DataType  elementAt(const int index) const;  // Access the element at the given index
-int  getSize() const;                      // Return the number of items in the vector
-DataType &operator[] (const int index);   // overloading subscript []
  */
 template class Vector<int>;
 template class Vector<float>;
--- Vector/main.cpp
+++ Vector/main.cpp
@@ -9,7 +9,20 @@
 #include <iostream>
 #include "Vector.hpp"
 using namespace std;
-//template <class DataType>
+
+template <class DataType>
+void print(Vector<DataType> v)
+{
+    int len = v.getSize();
+    for(int i = 0; i < len; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+}
+template class Vector<int>;
+template class Vector<float>;
+template class Vector<char>;
 
 int main(int argc, const char * argv[]) {
     cout << "Program start!\n";
@@ -26,6 +39,46 @@ int main(int argc, const char * argv[]) {
      TESTING ELEMENT_AT
      */
     cout << "~~~~~~~~~~~~~~~~~~\n" << "TESTING ELEMENT_AT\n";
-    cout << "v[0] : " << v.elementAt(0) << " v[2] : " << v.elementAt(3) << endl;
+    cout << "v[0] : " << v.elementAt(0) <</* " v[4] : " << v.elementAt(4) <<*/ endl;
+    /**
+     TESTING [] OPERATOR
+     */
+    cout << "~~~~~~~~~~~~~~~~~~\n" << "TESTING [] OPERATOR\n";
+    cout << "v[0] : " << v[0] <</* " v[4] : " << v[4] <<*/ endl;
+    /**
+     TESTING INSERT AT
+     */
+    cout << "~~~~~~~~~~~~~~~~~~\n" << "TESTING TESTING INSERT AT\n";
+    v.insertAt(0,1);
+    v.insertAt(1,2);
+    print(v);
+    /**
+     TESTING REMOVE AND REMOVE AT
+     */
+    cout << "~~~~~~~~~~~~~~~~~~\n" << "TESTING REMOVE AND REMOVE AT\n";
+    v.removeAt(0);
+    v.remove(1);
+    print(v);
+    /**
+     TESTING CONTAINS,  IS FULL, CLEAR,  IS EMPTY
+     */
+    cout << "~~~~~~~~~~~~~~~~~~\n" << "TESTING CONTAINS, IS FULL, CLEAR, IS EMPTY\n";
+    cout << "v.contains(777) : " << v.contains(777) << endl;
+    cout << "v.contains(-1) : " << v.contains(-1) << endl;
+    v.clear();
+    v.append(0);
+    v.append(1);
+    cout << "v.isFull() : " << v.isFull() << endl;
+    print(v);
+    v.clear();
+    cout << "v.isEmpty() : " << v.isEmpty() << endl;
+    print(v);
+    
+    
+    
+    
+    
+    
+    
     return 0;
 }
